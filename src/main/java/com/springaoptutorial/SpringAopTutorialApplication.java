ff6240b2b40bef837150df3994488fba67a33d71
krishnaSarkar5 <krishnasarkar95@gmail.com>
2023-03-08 12:48:37 +0530

    first commit

---
+++ src/main/java/com/springaoptutorial/SpringAopTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.springaoptutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopTutorialApplication.class, args);
+	}
+
+}
