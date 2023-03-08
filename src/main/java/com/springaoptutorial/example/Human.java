b6fb783679b1be5e139fb2423f4e96458ecaf8e8
krishnaSarkar5 <krishnasarkar95@gmail.com>
2023-03-08 18:23:51 +0530

    @PointCut added

--- src/main/java/com/springaoptutorial/example/Human.java
+++ src/main/java/com/springaoptutorial/example/Human.java
@@ -1,7 +1,9 @@
 package com.springaoptutorial.example;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +13,22 @@ import org.springframework.stereotype.Component;
 //@EnableAspectJAutoProxy
 public class Human {
 
-    @Before(value = "execution(public void study())")
+
+    // define pointCuts in one place and use it multiple times
+
+
+    @Pointcut("execution(public void study())")
+    public void myPointCuts(){}
+
+
+    @Before("myPointCuts()")
     public void wakeUp(){
         System.out.println("Good Morning !!!");
+    }
+
+
+    @After("myPointCuts()")
+    public void sleep(){
+        System.out.println("Good Night !!!");
     }
 }
